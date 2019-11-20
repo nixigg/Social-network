614b0618a92b5834df8c18badcd15612852d3967
Sergiu <nixigg@gmail.com>
2019-11-20 00:11:53 -0800

    Frontend part (React) navbar, user signup and signin

---
+++ react-front/src/core/Menu.js
@@ -0,0 +1,74 @@
+import React from "react";
+import { Link, withRouter } from "react-router-dom";
+import { signout, isAuthenticated } from "../auth/index";
+
+const isActive = (history, path) => {
+  if (history.location.pathname === path) {
+    return { color: "#ff9900" };
+  } else {
+    return { color: "#ffffff" };
+  }
+};
+
+const Menu = ({ history }) => {
+  return (
+    <div>
+      <ul className="nav nav-tabs bg-primary">
+        <li className="nav-item">
+          <Link className="nav-link" style={isActive(history, "/")} to="/">
+            Home
+          </Link>
+        </li>
+        {!isAuthenticated() && (
+          <>
+            <li className="nav-item">
+              <Link
+                className="nav-link"
+                style={isActive(history, "/signin")}
+                to="/signin"
+              >
+                Sign In
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                className="nav-link"
+                style={isActive(history, "/signup")}
+                to="/signup"
+              >
+                Sign Up
+              </Link>
+            </li>
+          </>
+        )}
+        {isAuthenticated() && (
+          <>
+            <li className="nav-item">
+              <a
+                className="nav-link"
+                style={
+                  (isActive(history, "/signup"),
+                  { cursor: "pointer", color: "#fff" })
+                }
+                onClick={() => signout(() => history.push("/"))}
+              >
+                Sign Out
+              </a>
+            </li>
+            <li className="nav-item">
+              <Link
+                className="nav-link"
+                to={`/user/${isAuthenticated().user._id}`}
+                style={{ color: "#fff" }}
+              >
+                {`${isAuthenticated().user.name}'s profile`}
+              </Link>
+            </li>
+          </>
+        )}
+      </ul>
+    </div>
+  );
+};
+
+export default withRouter(Menu);
